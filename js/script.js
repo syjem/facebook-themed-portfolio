235ab1065316293ca9d6eadaff795d4130f97d07
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-21 10:20:22 +0800

    random color functionality

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
 
-var mainNav = document.querySelector(".main-nav");
-var links = mainNav.getElementsByTagName("a");
+window.onload = function() {
 
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+  var mainNav = document.querySelector(".main-nav");
+  var links = mainNav.getElementsByTagName("a");
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+
+  // Stop Scrolling
+
+  $(document).ready(function() {
+    $("#aside-left").scroll(function() {
+      if ($("#aside-left").scrollTop() + $("#aside-left").innerHeight() >= $("#aside-left")[0].scrollHeight) {
+        $("#aside-left").addClass("stop-scroll");
+      } else {
+        $("#aside-left").removeClass("stop-scroll");
+      }
+    });
   });
+
+
+  // Changed background Color on click
+
+    const changeBtn = document.getElementById("fb_btn");
+    const htmlBody = document.querySelector("body");
+
+    const randomFunction = function() {
+    const colors = ["#FFB84C", "#F16767", "#A459D1", "#EB455F", "#B4E4FF", "#F0A04B", "#EA8FEA"];
+
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+
+    htmlBody.style.backgroundColor = randomColor;
+
+  }
+
+  changeBtn.onclick = randomFunction;
+
 }
 
 
-// Stop Scrolling
-
-$(document).ready(function() {
-  $("#aside-left").scroll(function() {
-    if ($("#aside-left").scrollTop() + $("#aside-left").innerHeight() >= $("#aside-left")[0].scrollHeight) {
-      $("#aside-left").addClass("stop-scroll");
-    } else {
-      $("#aside-left").removeClass("stop-scroll");
-    }
-  });
-});
