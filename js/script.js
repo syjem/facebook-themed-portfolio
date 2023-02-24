0ca567719cdcd442663ff6eb1c44ab2e6602ea94
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-24 22:03:16 +0800

    deleted the errors

--- js/script.js
+++ js/script.js
@@ -1,31 +1,6 @@
 
 window.onload = function() {
 
-  var mainNav = document.querySelector(".main-nav");
-  var links = mainNav.getElementsByTagName("a");
-
-  for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-    });
-  }
-
-
-  // Stop Scrolling
-
-  $(document).ready(function() {
-    $("#aside-left").scroll(function() {
-      if ($("#aside-left").scrollTop() + $("#aside-left").innerHeight() >= $("#aside-left")[0].scrollHeight) {
-        $("#aside-left").addClass("stop-scroll");
-      } else {
-        $("#aside-left").removeClass("stop-scroll");
-      }
-    });
-  });
-
-
   // Changed background Color on click
 
     const changeBtn = document.getElementById("fb_btn");
