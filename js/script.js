8dd1357cc5e7450c444874e47a446494bb9e5ab9
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-26 21:15:51 +0800

    remove border when changing color

--- js/script.js
+++ js/script.js
@@ -18,24 +18,31 @@ window.onload = function() {
    switch(randomColor) {
     case "#EB455F":
       postBtn.style.backgroundColor = "#EB455F";
+      postBtn.style.border = "none";
       break;
     case "#FF7B54":
       postBtn.style.backgroundColor = "#FF7B54";
+      postBtn.style.border = "none";
       break;
     case "#D3756B":
       postBtn.style.backgroundColor = "#D3756B";
+      postBtn.style.border = "none";
       break;
     case "#FEA1BF":
       postBtn.style.backgroundColor = "#FEA1BF";
+      postBtn.style.border = "none";
       break;
     case "#C92C6D":
       postBtn.style.backgroundColor = "#C92C6D";
+      postBtn.style.border = "none";
       break;
     case "#D61355":
       postBtn.style.backgroundColor =  "#D61355";
+      postBtn.style.border = "none";
       break;
     case "#CB1C8D":
       postBtn.style.backgroundColor =  "#CB1C8D";
+      postBtn.style.border = "none";
       break;
     case "#18191a":
       postBtn.style.backgroundColor = "#23bbe1";
