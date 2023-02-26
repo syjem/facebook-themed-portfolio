e7f66345a27a11cd792ca3d87aab1a1cb81cc5ff
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-02-26 23:41:51 +0800

    jQuery for slider modal

--- js/jquery.js
+++ js/jquery.js
@@ -32,5 +32,44 @@ $(function() {
 
     // Add a border to post button
     $("#share").css("border", "1px solid #054453")
+
+
+    // Slider images
+
+    var slideIndex = 1;
+    showSlides(slideIndex);
+    
+    // Next/previous controls
+    $("#prev").on("click", function() {
+        showSlides(slideIndex -= 1);
+    });
+    $("#next").on("click", function() {
+        showSlides(slideIndex += 1);
+    });
+    
+    function showSlides(n) {
+        var i;
+        var slides = $("#images-slider img");
+        if (n > slides.length) {
+        slideIndex = 1
+        }
+        if (n < 1) {
+        slideIndex = slides.length
+        }
+        slides.hide();
+        slides.eq(slideIndex - 1).show();
+    }
+
+
+    // AJAX slider
+
+    $(".mini").on("click", function() {
+        $("#slider-box").show();
+    });
+
+    $("#x-mark").on("click", function() {
+        $("#slider-box").hide();
+    });
+    
 });
 
